64199ce2a17158f08a51746482e191a4beb4b7b3
InM1nd <zabolotny1111@gmail.com>
2025-04-01 00:38:08 +0200

    Упрощена конфигурация Next.js и добавлен vercel.json для решения проблемы сборки

--- next.config.js
+++ next.config.js
@@ -1,30 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: "all",
-          minSize: 20000,
-          maxSize: 244000,
-          minChunks: 1,
-          maxAsyncRequests: 30,
-          maxInitialRequests: 30,
-        },
-      };
-    }
-    return config;
-  },
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "api.dicebear.com",
-      },
-      // Добавьте сюда другие конкретные домены, если они используются
-    ],
+    domains: ["api.dicebear.com"],
+    unoptimized: true,
   },
 };
 
