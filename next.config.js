454ea522483f721de196358c51ecf30105197cf4
InM1nd <zabolotny1111@gmail.com>
2025-04-01 00:34:31 +0200

    Исправлена конфигурация remotePatterns для решения ошибки с максимальным размером стека на Vercel

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // Минимальная конфигурация для оптимизации изображений
-  images: {
-    domains: ["example.com"], // Замени на нужные домены, если используешь изображения из интернета
-  },
-
-  // Если у тебя нет кастомного Webpack-конфига, можно его временно убрать
   webpack: (config, { isServer }) => {
-    // Можно оставить для оптимизации в случае сборки на сервере
     if (!isServer) {
       config.optimization = {
         ...config.optimization,
@@ -23,15 +15,16 @@ const nextConfig = {
         },
       };
     }
-
     return config;
   },
-
-  // Убедимся, что мы не настраиваем лишние или экспериментальные параметры
-  // Временно отключим любые экспериментальные флаги, если они есть
-  experimental: {
-    // Пример, если что-то было настроено
-    // ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.dicebear.com",
+      },
+      // Добавьте сюда другие конкретные домены, если они используются
+    ],
   },
 };
 
