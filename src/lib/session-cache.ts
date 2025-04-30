0af478ff70878b863d17692d73caa63d2c4d7a4f
InM1nd <zabolotny1111@gmail.com>
2025-04-30 18:49:39 +0200

    Some new features

--- src/lib/session-cache.ts
+++ src/lib/session-cache.ts
@@ -5,7 +5,7 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 1000 * 60 * 5, // 5 минут
-      cacheTime: 1000 * 60 * 30, // 30 минут
+      gcTime: 1000 * 60 * 30, // 30 минут
     },
   },
 });
